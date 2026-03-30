513dd41503790aaf675b6dd15e98a23575578e2f
Muhammad-Mashhood <139689918+Muhammad-Mashhood@users.noreply.github.com>
2026-03-30 10:26:32 +0500

    Migrate demo_app/models.py to Django 5

--- demo_app/models.py
+++ demo_app/models.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models import Index, UniqueConstraint
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
+    
+    class Meta:
+        indexes = [Index(fields=['name'])]
+        constraints = [UniqueConstraint(fields=['name'], name='unique_author_name')]
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    class Meta:
+        indexes = [Index(fields=['title'])]
+        constraints = [UniqueConstraint(fields=['title', 'author'], name='unique_book_title_author')]
