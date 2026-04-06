bf7f4b4f827478cef6bd974a3db399b2a6a6db21
Muhammad-Mashhood <139689918+Muhammad-Mashhood@users.noreply.github.com>
2026-04-06 07:32:45 +0500

    Migrate demo_app/models.py to Django 5

--- demo_app/models.py
+++ demo_app/models.py
@@ -2,9 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
-@python_2_unicode_compatible
 class Author(models.Model):
     name = models.CharField(max_length=100)
     
@@ -13,5 +11,4 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    # Missing on_delete parameter! (Django 2.0+ requires it)
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
